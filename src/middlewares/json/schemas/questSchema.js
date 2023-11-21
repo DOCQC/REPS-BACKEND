36a661938a0ce095870f5d9893b6d460bdafece2
SaldanhaElisson <elissonsaldanha19@gmail.com>
2023-11-21 20:50:15 -0300

    fix(DOCB006): add propertie on questSchemas

--- src/middlewares/json/schemas/questSchema.js
+++ src/middlewares/json/schemas/questSchema.js
@@ -1,13 +1,20 @@
-export const questShema = {
+export const questShcema = {
     title: "Quests schemas",
     descriotion: "Need title, description, enterprise and area of expertise",
     type: "object",
     properties:{
         "title": {
-            "title": "title of quest",
+            "description": "title of quest",
             "type":"string"
         },
-        
+        "description" :{
+            "description" : "description of quest",
+            "type" : "string"
+        },
+        "enterpriseId":{
+            "description": "enterprise id",
+            "type" : "number"
+        }
 
     }
 }

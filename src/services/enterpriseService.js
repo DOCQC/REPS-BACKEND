dba40178672dbc84de3e9c697dcb965f1a40af2c
SaldanhaElisson <elissonsaldanha19@gmail.com>
2023-11-28 13:40:01 -0300

    remove bug

--- src/services/enterpriseService.js
+++ src/services/enterpriseService.js
@@ -39,11 +39,6 @@ export async function findAll(data) {
     return prisma.enterprise.findMany(data)
 }
 
-
-export async function create(data) {
-    const ENTERPRISE = Number(process.env.ROLE_ENTERPRISE)
-
-
 export async function create(data) {
     const ENTERPRISE = Number(process.env.ROLE_ENTERPRISE)
     return await prisma.enterprise.create({
